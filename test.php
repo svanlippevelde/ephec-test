0ea66d8b4f0bcf68c5fb38804fc343484b1068e5
Stéphane Van Lippevelde <stephane@vanlippevelde.be>
2020-10-15 17:50:52 +0200

    Project init

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php
+
+try {
+
+    $bdd = new PDO('mysql:host=127.0.0.1;dbname=profephec_hauteeco;charset=utf8', 'profephec_hauteeco', 'iaAPOdDff0sk');
+
+}
+catch (Exception $e)
+
+{
+
+    die('Erreur : ' . $e->getMessage());
+
+}
+
+    $reponse = $bdd->query('SELECT * from professeur');
+
+    while ($ligne = $reponse->fetch()){
+
+
+        echo 'Le professeur '.$ligne['nom'].' '.$ligne['prenom'].' a comme adresse : '.$ligne['adresse'].'</br>';
+
+
+    }
+
+    $reponse->closeCursor();
+
+
+    ?>
